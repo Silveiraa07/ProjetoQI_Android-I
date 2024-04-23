8969a33d9a3d2ab3d07449a8b6c5e71fa29ab945
Antônio Terhorst <antonio.terhorst@outlook.com>
2024-04-23 11:24:49 -0300

    Envio

--- app/src/main/java/com/silveira/projetoqi_boxes/MainActivity.kt
+++ app/src/main/java/com/silveira/projetoqi_boxes/MainActivity.kt
@@ -7,17 +7,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.silveira.projetoqi_boxes.ui.theme.ProjetoQI_BoxesTheme
+import androidx.compose.ui.unit.dp
+import com.silveira.projetoqi_boxes.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProjetoQI_BoxesTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Home()
@@ -33,12 +38,19 @@ fun Home() {
     Scaffold(
         topBar = {
             TopAppBar(
+                modifier = Modifier.shadow(8.dp),
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
+                    containerColor = Color(0xffF9F1F9)
                 ),
                 title = {
                     Text("Lista de tarefas")
+                },
+                actions = {
+                    IconButton(
+                        onClick = {},
+                    ){
+                        Icon(Icons.Default.MoreVert,"Tres pontos")
+                    }
                 }
             )
         },
@@ -53,7 +65,7 @@ fun Home() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ProjetoQI_BoxesTheme {
+    AppTheme {
         Home()
     }
 }
