e8f7d79ccc7d22b389494982c4c5167c1d844327
Antônio Terhorst <antonio.terhorst@outlook.com>
2024-04-25 12:17:05 -0300

    Envio 2

--- app/src/main/java/com/silveira/projetoqi_boxes/MainActivity.kt
+++ app/src/main/java/com/silveira/projetoqi_boxes/MainActivity.kt
@@ -3,17 +3,19 @@ package com.silveira.projetoqi_boxes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.silveira.projetoqi_boxes.ui.theme.AppTheme
@@ -48,16 +50,49 @@ fun Home() {
                 actions = {
                     IconButton(
                         onClick = {},
-                    ){
-                        Icon(Icons.Default.MoreVert,"Tres pontos")
+                    ) {
+                        Icon(Icons.Default.MoreVert, "Tres pontos", tint = Color.Black)
                     }
                 }
             )
         },
-    ) {
-        innerPading ->
+    ) { innerPading ->
         LazyColumn(Modifier.fillMaxSize().padding(innerPading)) {
+            item {
+                GerenCard()
+            }
+        }
+    }
+}
 
+@Composable
+fun GerenCard() {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .padding(20.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Lavar Louça",
+                modifier = Modifier
+                    .padding(16.dp),
+                textAlign = TextAlign.Center,
+            )
+            Row {
+                Checkbox(
+                    checked = false,
+                    onCheckedChange = {}
+                )
+                IconButton(onClick = {}){
+                    Icon(Icons.Default.Delete,"")
+                }
+            }
         }
     }
 }
@@ -67,5 +102,6 @@ fun Home() {
 fun GreetingPreview() {
     AppTheme {
         Home()
+        GerenCard()
     }
 }
